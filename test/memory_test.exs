5c6b8045e518ee82ecb9bc158f75a82a7bebb5bb
Kareem Kouddous <kareemknyc@gmail.com>
2018-07-17 19:45:01 -0400

    Dequeue in separate process for better GC

--- test/memory_test.exs
+++ test/memory_test.exs
@@ -21,7 +21,7 @@ defmodule MemoryTest do
   end
 
   @tag timeout: :infinity
-  test "test memory bloat" do
+  test "test scheduler memory bloat" do
     starting_memory = :erlang.memory(:total)
 
     Process.register(self(), :tester)
@@ -29,6 +29,33 @@ defmodule MemoryTest do
     {:ok, sup} = Exq.start_link(scheduler_enable: true)
     for _ <- 1..10_000, do: Exq.enqueue_in(Exq, "default", 5, MemoryTest.Worker, [Enum.reduce(1..10_000, "", fn _, acc -> acc <> "." end)])
     Exq.enqueue_in(Exq, "default", 5, MemoryTest.Worker, ["last"])
+
+    # Wait for last message
+    receive do
+      :done -> Logger.info("Received done")
+    end
+
+    :timer.sleep(1000)
+
+    memory_used  = :erlang.memory(:total) - starting_memory
+    Logger.debug "Memory used #{memory_used / (1_024 * 1024)} mb"
+    count = Exq.Redis.Connection.zcard!(:testredis, "test:schedule")
+
+    assert count == 0
+    assert memory_used < 50 * (1_024 * 1_024) # 50mb
+    # let stats finish
+    stop_process(sup)
+  end
+
+  @tag timeout: :infinity
+  test "test memory bloat" do
+    starting_memory = :erlang.memory(:total)
+
+    Process.register(self(), :tester)
+
+    {:ok, sup} = Exq.start_link(concurrency: 5)
+    for _ <- 1..10_000, do: Exq.enqueue(Exq, "default",  MemoryTest.Worker, [Enum.reduce(1..10_000, "", fn _, acc -> acc <> "." end)])
+    Exq.enqueue(Exq, "default", MemoryTest.Worker, ["last"])
 
     # Wait for last message
     receive do
