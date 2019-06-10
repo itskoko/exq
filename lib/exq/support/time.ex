36f0340f7954a22314fef0f6f242b5eab8109e26
Kareem Kouddous <kareem.kouddous@airbnb.com>
2019-06-10 16:27:43 -0700

    Fix for Elixir 1.8

--- lib/exq/support/time.ex
+++ lib/exq/support/time.ex
@@ -2,10 +2,10 @@ defmodule Exq.Support.Time do
   import DateTime, only: [utc_now: 0, to_unix: 2, from_unix!: 2]
 
   def offset_from_now(offset) do
-    now_micro_sec = utc_now() |> to_unix(:microseconds)
+    now_micro_sec = utc_now() |> to_unix(:microsecond)
     now = now_micro_sec
 
-    from_unix!(round(now + offset * 1_000_000), :microseconds)
+    from_unix!(round(now + offset * 1_000_000), :microsecond)
   end
 
   def time_to_score(time \\ utc_now()) do
@@ -15,7 +15,7 @@ defmodule Exq.Support.Time do
   end
 
   def unix_seconds(time \\ utc_now()) do
-    to_unix(time, :microseconds) / 1_000_000.0
+    to_unix(time, :microsecond) / 1_000_000.0
   end
 
   def format_current_date(current_date) do
